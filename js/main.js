7ed70207ee7dca84978717b13fa8fa15b674332b
Jimmy Tran <quantrannhu@gmail.com>
2025-04-09 03:53:17 +0000

    Completed issue 2

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 'use strict';
 const $form = document.querySelector('.form');
-if (!$form) throw new Error('Unable to find form element');
+if (!$form) throw new Error('Unable to query form element');
 const $entryImage = document.querySelector('.entry-image');
-if (!$entryImage) throw new Error('Unable to find entry-image element');
+if (!$entryImage) throw new Error('Unable to query entry-image element');
 const $formELements = $form.elements;
 const $photoURL = $formELements.photoURL;
 $photoURL.addEventListener('input', () => {
@@ -21,7 +21,87 @@ $form.addEventListener('submit', (event) => {
   const entry = { entryId, title, photoURL, notes };
   data.entries.unshift(entry);
   $entryImage.setAttribute('src', 'images/placeholder-image-square.jpg');
-  console.log(data);
   writeDataToLocalStorage();
+  // Update submit event callback function for ISSUE 2
+  const $newEntryItem = renderEntry(entry);
+  const $entryList = document.querySelector('.entry-list');
+  if (!$entryList) throw new Error('Unable to query entry-list element');
+  $entryList.prepend($newEntryItem);
+  viewSwap('entries');
+  toggleNoEntries('off');
+  // reset form after all done
   $form.reset();
+});
+// ISSUE 2
+function renderEntry(entry) {
+  const $entryElement = document.createElement('li');
+  $entryElement.className = 'entry';
+  const entryContent = `
+            <div class="row">
+              <div class="column-half">
+                <img
+                  class="view-entry-image"
+                  src=${entry.photoURL}
+                  alt="" />
+              </div>
+
+              <div class="column-half">
+                <div class="row">
+                  <h3 class="entry-title">${entry.title}</h3>
+                </div>
+
+                <p class="entry-notes">${entry.notes}
+                </p>
+              </div>
+            </div>`;
+  $entryElement.innerHTML = entryContent;
+  return $entryElement;
+}
+document.addEventListener('DOMContentLoaded', () => {
+  // load the list items
+  const $entryList = document.querySelector('.entry-list');
+  if (!$entryList) throw new Error('Unable to query entry-list element');
+  if (data.entries.length > 0) {
+    toggleNoEntries('off');
+    data.entries.forEach((entry) => {
+      const $entryListItem = renderEntry(entry);
+      $entryList.appendChild($entryListItem);
+    });
+  }
+  // show the previous view before refresh
+  viewSwap(data.view);
+});
+function toggleNoEntries(state) {
+  const $noEntryAnnouncement = document.querySelector('.no-entry');
+  if (!$noEntryAnnouncement)
+    throw new Error('Unable to query no-entry element');
+  switch (state) {
+    case 'on':
+      $noEntryAnnouncement.classList.remove('hidden');
+      break;
+    case 'off':
+      $noEntryAnnouncement.classList.add('hidden');
+      break;
+  }
+}
+function viewSwap(view) {
+  const $dataViews = document.querySelectorAll('[data-view]');
+  if (!$dataViews) throw new Error('Unable to query data-view elements');
+  $dataViews.forEach((item) =>
+    item.dataset.view === view
+      ? item.classList.remove('hidden')
+      : item.classList.add('hidden'),
+  );
+  data.view = view;
+  writeDataToLocalStorage(); // everytime data change, save it to local storage
+}
+const $entriesLink = document.querySelector('.entries-link');
+if (!$entriesLink) throw new Error('Unable to query entries element');
+$entriesLink.addEventListener('click', () => {
+  viewSwap('entries');
+});
+const $buttonNew = document.querySelector('.button-new');
+if (!$buttonNew) throw new Error('Unable to query button-new element');
+$buttonNew.addEventListener('click', () => {
+  viewSwap('entry-form');
 });
--- ts/main.ts
+++ ts/main.ts
@@ -1,3 +1,4 @@
+// ISSUE 1
 interface FormElements extends HTMLFormControlsCollection {
   title: HTMLInputElement;
   photoURL: HTMLInputElement;
@@ -12,9 +13,9 @@ interface Entry {
 }
 
 const $form = document.querySelector('.form') as HTMLFormElement;
-if (!$form) throw new Error('Unable to find form element');
+if (!$form) throw new Error('Unable to query form element');
 const $entryImage = document.querySelector('.entry-image') as HTMLImageElement;
-if (!$entryImage) throw new Error('Unable to find entry-image element');
+if (!$entryImage) throw new Error('Unable to query entry-image element');
 const $formELements = $form.elements as FormElements;
 const $photoURL = $formELements.photoURL;
 
@@ -36,5 +37,98 @@ $form.addEventListener('submit', (event: Event) => {
   data.entries.unshift(entry);
   $entryImage.setAttribute('src', 'images/placeholder-image-square.jpg');
   writeDataToLocalStorage();
+
+  // Update submit event callback function for ISSUE 2
+  const $newEntryItem: HTMLLIElement = renderEntry(entry);
+  const $entryList = document.querySelector('.entry-list');
+  if (!$entryList) throw new Error('Unable to query entry-list element');
+  $entryList.prepend($newEntryItem);
+  viewSwap('entries');
+  toggleNoEntries('off');
+
+  // reset form after all done
   $form.reset();
+});
+
+// ISSUE 2
+function renderEntry(entry: Entry): HTMLLIElement {
+  const $entryElement = document.createElement('li');
+  $entryElement.className = 'entry';
+  const entryContent: string = `
+            <div class="row">
+              <div class="column-half">
+                <img
+                  class="view-entry-image"
+                  src=${entry.photoURL}
+                  alt="" />
+              </div>
+
+              <div class="column-half">
+                <div class="row">
+                  <h3 class="entry-title">${entry.title}</h3>
+                </div>
+
+                <p class="entry-notes">${entry.notes}
+                </p>
+              </div>
+            </div>`;
+  $entryElement.innerHTML = entryContent;
+  return $entryElement;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  // load the list items
+  const $entryList = document.querySelector('.entry-list');
+  if (!$entryList) throw new Error('Unable to query entry-list element');
+  if (data.entries.length > 0) {
+    toggleNoEntries('off');
+    data.entries.forEach((entry) => {
+      const $entryListItem: HTMLLIElement = renderEntry(entry);
+      $entryList.appendChild($entryListItem);
+    });
+  }
+  // show the previous view before refresh
+  viewSwap(data.view);
+});
+
+function toggleNoEntries(state: string): void {
+  const $noEntryAnnouncement = document.querySelector('.no-entry');
+  if (!$noEntryAnnouncement)
+    throw new Error('Unable to query no-entry element');
+  switch (state) {
+    case 'on':
+      $noEntryAnnouncement.classList.remove('hidden');
+      break;
+    case 'off':
+      $noEntryAnnouncement.classList.add('hidden');
+      break;
+  }
+}
+
+function viewSwap(view: string): void {
+  const $dataViews = document.querySelectorAll(
+    '[data-view]',
+  ) as NodeListOf<HTMLDivElement>;
+  if (!$dataViews) throw new Error('Unable to query data-view elements');
+  $dataViews.forEach((item) =>
+    item.dataset.view === view
+      ? item.classList.remove('hidden')
+      : item.classList.add('hidden'),
+  );
+  data.view = view;
+  writeDataToLocalStorage(); // everytime data change, save it to local storage
+}
+
+const $entriesLink = document.querySelector(
+  '.entries-link',
+) as HTMLAnchorElement;
+if (!$entriesLink) throw new Error('Unable to query entries element');
+$entriesLink.addEventListener('click', () => {
+  viewSwap('entries');
+});
+
+const $buttonNew = document.querySelector('.button-new') as HTMLAnchorElement;
+if (!$buttonNew) throw new Error('Unable to query button-new element');
+$buttonNew.addEventListener('click', () => {
+  viewSwap('entry-form');
 });
